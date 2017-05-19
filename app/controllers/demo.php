f153c07d6440a49eaf01909f7fccddc38e6fd80a
xinson <xinliu515352@sina.com>
2017-05-19 11:32:57 +0800

    view class

--- app/controllers/demo.php
+++ app/controllers/demo.php
@@ -21,31 +21,22 @@ class demo
         $log->error('Bar');
         */
 
-        /*
-        $path = [PATH.'/app/templates'];         // 视图文件目录，这是数组，可以有多个目录
-        $cachePath = PATH.'/storage/framework/cache';     // 编译文件缓存目录
-        $compiler = new \Xiaoler\Blade\Compilers\BladeCompiler($cachePath);
-        // 如过有需要，你可以添加自定义关键字
-        $compiler->directive('datetime', function($timestamp) {
-            return preg_replace('/(\(\d+\))/', '<?php echo date("Y-m-d H:i:s", $1); ?>', $timestamp);
-        });
-        $engine = new \Xiaoler\Blade\Engines\CompilerEngine($compiler);
-        $finder = new \Xiaoler\Blade\FileViewFinder($path);
-        // 如果需要添加自定义的文件扩展，使用以下方法
-        $finder->addExtension('tpl');
-        // 实例化 Factory
-        $factory = new \Xiaoler\Blade\Factory($engine, $finder);
-        // 渲染视图并输出
-        echo $factory->make('index', ['a' => 1, 'b' => 2])->render();
-        */
-
-        //
         $path = [PATH.'/app/templates'];         // 视图文件目录，这是数组，可以有多个目录
         $cachePath = PATH.'/storage/framework/cache';     // 编译文件缓存目录
         $compiler = new \App\Framework\View\Compiler\BladeCompiler($cachePath);
-
-        $compiler = new \Xiaoler\Blade\Compilers\BladeCompiler($cachePath);
-
+        /*
+         $compiler->directive('datetime', function($timestamp) {
+            return preg_replace('/(\(\d+\))/', '<?php echo date("Y-m-d H:i:s", $1); ?>', $timestamp);
+         });
+         */
+        $engine = new \App\Framework\View\Engines\CompilerEngine($compiler);
+        $finder = new \App\Framework\View\FileViewFinder($path);
+        // 如果需要添加自定义的文件扩展，使用以下方法
+        //$finder->addExtension('tpl');
+        // 实例化 Factory
+        $factory = new \App\Framework\View\Factory($engine, $finder);
+        // 渲染视图并输出
+        echo $factory->make('index', ['a' => 1, 'b' => 2])->render();
 
     }
 
