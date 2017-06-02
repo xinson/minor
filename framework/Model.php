81132b165c67d94e1603a6d8e61c59b758407ece
xinson <xinliu515352@sina.com>
2017-06-02 19:28:58 +0800

    Model

--- framework/Model.php
+++ framework/Model.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Framework;
 
 use App\Framework\Db\MysqlPdo;
@@ -6,48 +7,67 @@ use App\Framework\Db\MysqlPdo;
 class Model
 {
     protected $pdo;
+    protected $table;
+    protected $primaryKey;
+    protected $exists;
+    protected $original = array();
     protected $data = array();
 
     public function __construct()
     {
-        try {
-            $pdo = new MysqlPdo();
-            $pdo->query();
-        }catch (\PDOException $exception){
-            echo '<pre>';
-            echo $exception->errorInfo;
-            echo $exception->getMessage();
+        $this->pdo = new MysqlPdo();
+    }
+
+    public function setData($key, $value = '')
+    {
+        if(!empty($key) && !empty($value)){
+            $this->data[$key] = $value;
+        }
+        return $this;
+    }
+
+    public function getData($key = '')
+    {
+        if(!empty($key)){
+            return array_key_exists($key,$this->data)?$this->data[$key]:'';
+        }else{
+            return $this->data;
         }
     }
 
-    public function setData()
+    public function setTable($table)
     {
-
+        if(!empty($table)){
+            $this->table = $table;
+        }
+        return $this;
     }
 
-    public function getData()
+    public function getPrimary($primaryKey)
     {
-
+        return $this->primaryKey = $primaryKey;
     }
 
-    public function setTable()
+    public function setPrimary($primaryKey)
     {
-
+        $this->primaryKey = $primaryKey;
+        return $this;
     }
 
-    public function getPrimary()
+    public function find($id)
     {
-
-    }
-
-    public function setPrimary()
-    {
-
-    }
-
-    public function find()
-    {
-
+        if(!empty($id)) {
+            $sql = " SELECT * FROM {$this->getTable()} WHERE {$this->getPrimary()} = :{$this->getPrimary()} ";
+            $array = $this->pdo->query($sql, array($this->getPrimary() => $id));
+            if(!empty($array) && !is_array($array)){
+                foreach ($array as $d => $v){
+                    $this->setData($d,$v);
+                }
+                $this->exists = true;
+                $this->original = $this->getData();
+            }
+        }
+        return $this->getData();
     }
 
     public function findAndWhere()
